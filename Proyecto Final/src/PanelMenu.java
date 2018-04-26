f30d601fe1b23344bfac2441d38d790df4a1b4d2
LepeCrazy13 <38668193+LepeCrazy13@users.noreply.github.com>
2018-04-25 20:58:20 -0500

    menu chido

--- Proyecto Final/src/PanelMenu.java
+++ Proyecto Final/src/PanelMenu.java
@@ -24,6 +24,13 @@ public class PanelMenu extends JPanel{
 	private JLabel Label1;
 	private JButton B1,
 					B2;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
+	private JLabel lblNewLabel_3;
+	private JLabel lblNewLabel_4;
+	private JLabel lblNewLabel_5;
+	private JLabel lblNewLabel_6;
 	
 	public PanelMenu() {
 		super();
@@ -49,8 +56,8 @@ public class PanelMenu extends JPanel{
 		B2.setFont(new Font("Tahoma", Font.PLAIN, 50));
 		
 		CurrentLayOut = new SpringLayout();
-		CurrentLayOut.putConstraint(SpringLayout.NORTH, B2, 0, SpringLayout.NORTH, B1);
-		CurrentLayOut.putConstraint(SpringLayout.EAST, B2, -93, SpringLayout.EAST, this);
+		CurrentLayOut.putConstraint(SpringLayout.WEST, B2, 0, SpringLayout.WEST, B1);
+		CurrentLayOut.putConstraint(SpringLayout.SOUTH, B2, -122, SpringLayout.SOUTH, this);
 		CurrentLayOut.putConstraint(SpringLayout.NORTH, B1, 89, SpringLayout.SOUTH, Label1);
 		CurrentLayOut.putConstraint(SpringLayout.WEST, B1, 74, SpringLayout.WEST, this);
 		CurrentLayOut.putConstraint(SpringLayout.NORTH, Label1, 28, SpringLayout.NORTH, this);
@@ -80,6 +87,48 @@ public class PanelMenu extends JPanel{
 		this.add(Label1);
 		this.add(B1);
 		this.add(B2);
+		
+		lblNewLabel = new JLabel("PASOS A SEGUIR:");
+		CurrentLayOut.putConstraint(SpringLayout.NORTH, lblNewLabel, 0, SpringLayout.NORTH, B1);
+		CurrentLayOut.putConstraint(SpringLayout.EAST, lblNewLabel, -137, SpringLayout.EAST, this);
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 30));
+		add(lblNewLabel);
+		
+		lblNewLabel_1 = new JLabel("<html>Llenar toda la informaci\u00F3n\r<br/>solicitada en \u00E9ste tr\u00E1mite</html>");
+		CurrentLayOut.putConstraint(SpringLayout.NORTH, lblNewLabel_1, 23, SpringLayout.SOUTH, lblNewLabel);
+		CurrentLayOut.putConstraint(SpringLayout.WEST, lblNewLabel_1, 0, SpringLayout.WEST, lblNewLabel);
+		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 20));
+		add(lblNewLabel_1);
+		
+		lblNewLabel_2 = new JLabel("<html>Presentarse con identifiaci\u00F3n solicitada,\r<br/>en el lugar y hora indicado,\r<br/>para tomarle las fotos y validar\r<br/>su informaci\u00F3n en persona<html>");
+		CurrentLayOut.putConstraint(SpringLayout.NORTH, lblNewLabel_2, 45, SpringLayout.SOUTH, lblNewLabel_1);
+		CurrentLayOut.putConstraint(SpringLayout.WEST, lblNewLabel_2, 0, SpringLayout.WEST, lblNewLabel);
+		lblNewLabel_2.setFont(new Font("Arial", Font.PLAIN, 20));
+		add(lblNewLabel_2);
+		
+		lblNewLabel_3 = new JLabel("<html>Recibir un correo con la informaci\u00F3n\r<br/>digitalizada que podr\u00E1 usar\r<br/>como documento oficial</html>");
+		CurrentLayOut.putConstraint(SpringLayout.NORTH, lblNewLabel_3, 47, SpringLayout.SOUTH, lblNewLabel_2);
+		CurrentLayOut.putConstraint(SpringLayout.WEST, lblNewLabel_3, 0, SpringLayout.WEST, lblNewLabel);
+		lblNewLabel_3.setFont(new Font("Arial", Font.PLAIN, 20));
+		add(lblNewLabel_3);
+		
+		lblNewLabel_4 = new JLabel("1.-");
+		lblNewLabel_4.setFont(new Font("Arial", Font.BOLD, 20));
+		CurrentLayOut.putConstraint(SpringLayout.NORTH, lblNewLabel_4, 0, SpringLayout.NORTH, lblNewLabel_1);
+		CurrentLayOut.putConstraint(SpringLayout.EAST, lblNewLabel_4, -35, SpringLayout.WEST, lblNewLabel_1);
+		add(lblNewLabel_4);
+		
+		lblNewLabel_5 = new JLabel("2.-");
+		lblNewLabel_5.setFont(new Font("Arial", Font.BOLD, 20));
+		CurrentLayOut.putConstraint(SpringLayout.NORTH, lblNewLabel_5, 0, SpringLayout.NORTH, lblNewLabel_2);
+		CurrentLayOut.putConstraint(SpringLayout.WEST, lblNewLabel_5, 0, SpringLayout.WEST, lblNewLabel_4);
+		add(lblNewLabel_5);
+		
+		lblNewLabel_6 = new JLabel("3.-");
+		lblNewLabel_6.setFont(new Font("Arial", Font.BOLD, 20));
+		CurrentLayOut.putConstraint(SpringLayout.NORTH, lblNewLabel_6, 0, SpringLayout.NORTH, lblNewLabel_3);
+		CurrentLayOut.putConstraint(SpringLayout.WEST, lblNewLabel_6, 0, SpringLayout.WEST, lblNewLabel_4);
+		add(lblNewLabel_6);
 		
 	}
 	
